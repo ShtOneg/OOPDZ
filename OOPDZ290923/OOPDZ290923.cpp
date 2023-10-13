7aa9ac1189b58231a9a17bf6f379bffc3881f282
ShtOneg <Sh0tnik.19@yandex.ru>
2023-10-13 11:55:40 +0300

    Добавьте файлы проекта.

---
+++ OOPDZ290923/OOPDZ290923.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <math.h>
+
+using namespace std;
+
+class Triangle {
+public:
+    float a;
+    float b;
+    float c;
+    bool equilateral;
+    Triangle(float a, float b, float c) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->equilateral = false;
+        if (a == b && b == c) {
+            this->equilateral = true;
+        }
+    };
+    Triangle(float a, float b) {
+        this->a = a;
+        this->b = b;
+        this->c = sqrt(pow(a, 2) + pow(b, 2));
+        this->equilateral = false;
+    };
+    float perimetr() {
+        if (equilateral) {
+            return a * 3;
+        }
+        return a + b + c;
+    };
+    void print() {
+        cout << "Сторона a: " << a << ", сторона b: " << b << ", cторона c: " << c << "\nПериметр: " << this->perimetr() << "\n\n";
+    };
+    bool operator == (Triangle& tr) {
+        if (this->perimetr() == tr.perimetr()) {
+            return true;
+        }
+        return false;
+    }
+    bool operator > (Triangle& tr) {
+        if (this->perimetr() > tr.perimetr()) {
+            return true;
+        }
+        return false;
+    }
+    bool operator < (Triangle & tr) {
+        if (this->perimetr() < tr.perimetr()) {
+            return true;
+        }
+        return false;
+    }
+
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Triangle tr1(6.25, 7.85, 9.65);
+    Triangle tr2(5, 7);
+    tr1.print();
+    tr2.print();
+    if (tr1 > tr2) {
+        cout << tr1.perimetr();
+    }
+    else if (tr1 < tr2) {
+        cout << tr2.perimetr();
+    }
+}
+
+
+
---
+++ OOPDZ4/OOPDZ4.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+class Component {
+public:
+    string name;
+    float price;
+    Component(string name, float price) {
+        this->name = name;
+        this->price = price;
+    };
+    void print() {
+        cout << "Название: " << this->name << ", Цена: " << this->price << endl;
+    };
+};
+
+class CPU : public Component {
+public:
+    string socket;
+    int cores;
+    CPU(string name, float price, string socket, int cores) : Component(name, price) {
+        this->name = name;
+        this->price = price;
+        this->socket = socket;
+        this->cores = cores;
+    };
+    void print() {
+        cout << "Название процессора: " << this->name << ", Цена: " << this->price << ", Сокет: " << this->socket << ", Кол-во ядер: " << this->cores << endl;
+    };
+};
+
+class RAM : public Component {
+public:
+    int volume;
+    int speed;
+    RAM(string name, float price, int volume, int speed) : Component(name, price) {
+        this->name = name;
+        this->price = price;
+        this->volume = volume;
+        this->speed = speed;
+    };
+    void print() {
+        cout << "Название ОЗУ: " << this->name << ", Цена: " << this->price << ", Обьём: " << this->volume << ", Скорость: " << this->speed << endl;
+    };
+};
+
+class GPU : public Component {
+public:
+    int memory;
+    int cores;
+    GPU(string name, float price, int memory, int cores) : Component(name, price) {
+        this->name = name;
+        this->price = price;
+        this->memory = memory;
+        this->cores = cores;
+    };
+    void print() {
+        cout << "Название видеокарты: " << this->name << ", Цена: " << this->price << ", Память: " << this->memory << ", КОл-во ядер: " << this->cores << endl;
+    };
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
+
+
